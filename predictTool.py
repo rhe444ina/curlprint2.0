c7d8a532313d1a71714448a10e6fe7cd5b413cdb
rhea <rhea.aaina@gmail.com>
2026-06-14 21:04:32 -0400

    Create predictTool.py

---
+++ predictTool.py
@@ -0,0 +1,19 @@
+from model import HairToolModel
+from generate_stl import create_comb_stl
+
+# Hair profilea
+new_user = [[3,2,2,0,1,1]]
+
+# Predicting dimensions
+spacing = 8.3
+length = 44.1
+angle = 29
+roundness = 0.95
+
+# Generating STL
+create_comb_stl(
+    spacing,
+    length,
+    angle,
+    roundness
+)
